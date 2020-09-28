e20a91a70059f5c034406f3777cf446ad247c550
Jacob Hearst <jhearst@protonmail.ch>
2020-09-28 15:52:28 -0700

    Add firebase

--- desktop_app/src/renderer.jsx
+++ desktop_app/src/renderer.jsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom'
 
 import MainPage from './components/MainPage.jsx'
 
+import * as firebase from 'firebase'
+
+const firebaseConfig = {} // TODO: Replace with real config
+firebase.initializeApp(firebaseConfig)
+
 console.log('Loaded React.')
 ReactDOM.render(
     <MainPage />,
